8dd4821c40ec100f6bc874c8fe1479ad04a13c90
cuifengbo <471363336@qq.com>
2023-08-28 09:56:42 +0800

    1

---
+++ src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
@@ -0,0 +1,20 @@
+//微信：egvh56ufy7hh ，QQ：821898835 
+package ltd.newbee.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+
+
+
+
+ */
+@MapperScan("ltd.newbee.mall.dao")
+@SpringBootApplication
+public class NewBeeMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeeMallApplication.class, args);
+    }
+}
---
+++ src/main/java/ltd/newbee/mall/common/IndexConfigTypeEnum.java
@@ -0,0 +1,28 @@
+
+package ltd.newbee.mall.common;
+
+/**
+
+
+
+
+ * @apiNote 首页配置项 1-搜索框热搜 2-搜索下拉框热搜 3-(首页)热销商品 4-(首页)新品上线 5-(首页)为你推荐
+ */
+public enum IndexConfigTypeEnum {
+
+    DEFAULT(0, "DEFAULT"),
+    INDEX_SEARCH_HOTS(1, "INDEX_SEARCH_HOTS"),
+    INDEX_SEARCH_DOWN_HOTS(2, "INDEX_SEARCH_DOWN_HOTS"),
+    INDEX_GOODS_HOT(3, "INDEX_GOODS_HOTS"),
+    INDEX_GOODS_NEW(4, "INDEX_GOODS_NEW"),
+    INDEX_GOODS_RECOMMOND(5, "INDEX_GOODS_RECOMMOND");
+
+    private int type;
+
+    private String name;
+
+    IndexConfig
+    public void setName(String name) {
+        this.name = name;
+    }
+}
